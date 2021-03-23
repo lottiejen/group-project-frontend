adb8e43fcf027e821697d21792952848c918a49e
Paul Dredge <dredgos@googlemail.com>
2021-03-23 17:11:34 +0000

    local state added to user input

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,14 +1,49 @@
 // User input to filter down the database results
 import Button from './Button';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const Input = () => {
+
+    const [players, setPlayers] = useState("2");
+    const [time, setTime] = useState("");
+    const [difficulty, setDifficulty] = useState("1");
+
+    const handlePlayers = (e) => {
+        setPlayers(e.currentTarget.value);
+    }
+
+    const handleTime = (e) => {
+        setTime(e.currentTarget.value);
+    }
+
+    const handleDifficulty = (e) => {
+        setDifficulty(e.currentTarget.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        let formData = {
+            "players": players,
+            "time": time,
+            "difficulty": difficulty, 
+        }
+
+        console.log(formData)
+    }
+
     return (
         <section className="d-flex flex-column align-items-center">
-            <form className="form-control d-flex flex-column  mb-4 searchform">
-                <h3>Game Finder</h3>
+            <h3>Game Finder</h3>
+            <form onSubmit={handleSubmit} className="form-control d-flex flex-column  mb-4 searchform">
                 <label htmlFor="players">Number of Players</label>
-                <select className="mb-4 py-1" id="players">
+                <select 
+                    value={players}
+                    onChange={handlePlayers} 
+                    className="mb-4 py-1" 
+                    id="players"
+                >
                     <option value="2">Two</option>
                     <option value="3">Three</option>
                     <option value="4">Four</option>
@@ -18,20 +53,30 @@ const Input = () => {
                     <option value="8">Eight</option>
                 </select>
                 <label htmlFor="time">Time (minutes)</label>
-                <input className="mb-4 search-input" type="number"></input>
+                <input
+                    value={time}
+                    onChange={handleTime} 
+                    className="mb-4 search-input" 
+                    type="number"
+                ></input>
                 <label htmlFor="difficulty">Difficulty</label>
-                <select className="mb-4 py-1" id="difficulty">
+                <select
+                    value={difficulty}
+                    onChange={handleDifficulty} 
+                    className="mb-4 py-1" 
+                    id="difficulty"
+                >
                     <option value="1">Easy</option> 
                     <option value="2">Medium</option> 
                     <option value="3">Hard</option> 
                 </select>
-                <Link to="/">
-                    <Button buttonClass="primary" buttonText="Find Me A Game"/>
-                </Link>
-                <Link to="/all">
-                    <Button buttonClass="primary" buttonText="Show All Games"/>
-                </Link>
+                
+                <Button buttonClass="primary" buttonText="Find Me A Game"/>
+                
             </form>
+            <Link to="/all">
+                <Button buttonClass="primary" buttonText="Show All Games"/>
+            </Link>
         </section>
     );
 };
