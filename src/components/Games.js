8a1b3fe36a7334f79e94cb30ad64dea79e1caadb
Lottie <lottiej17@gmail.com>
2021-03-23 14:37:34 +0000

    Created games pages styling

--- src/components/Games.js
+++ src/components/Games.js
@@ -6,22 +6,24 @@ let gamesData = manyGames.data;
 
 const Games = () => {
     return (
-        <section>
+        <section class="row card-container">
             {gamesData.map((game, index) => (                
-                <div key={index}>
+                <article className="gamelisting col-md-6" key={index}>
                     <h2>{game.title}</h2>
+                
                     <ul>
                         <li>Difficulty: {game.difficulty}</li>
                         <li>Time: {game.time}</li>
                         <li>Minimum Players: {game.min_players}</li>
                         <li>Maximum Players: {game.max_players}</li>
                     </ul>
+                    
                     <ul>
                         {game.genres.map ((genre, index) => (
                             <li key={index}>{genre}</li>
                         ))}
                     </ul>
-                </div>
+                </article>
             ))}
         </section>
     );
